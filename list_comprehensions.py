56e437a42fde63a884af85ab14b4963d8463ac26
Rafael Claudio <c.rafaelneto7@gmail.com>
2019-03-21 22:46:13 -0300

    Exercicios dias

---
+++ list_comprehensions.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    array = []
+    p = 0
+    for one in range(x + 1):
+        for two in range(y + 1):
+            for three in range(z + 1):
+                if one + two + three != n: 
+                    array.append([]) 
+                    array[p] = [one, two, three] 
+                    p += 1 
+    print(array)
